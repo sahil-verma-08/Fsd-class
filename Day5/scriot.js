51813628f41a907380bca0949c06351549bad0ee
sahil-verma-08 <sahilverma2431@gmail.com>
2024-10-15 15:30:53 +0530

    sahil

---
+++ Day5/scriot.js
@@ -0,0 +1,31 @@
+// let x=confirm(" are you sure?")
+// alert(x)
+
+// let v=prompt("enter your name","rahul")
+// alert(v)
+
+// let display=()=>{
+//     console.log("welcom")
+// };
+// display();
+
+// let sum=(x,y)=>{
+//     return x+y
+// };
+// let re=sum(5,6)
+// console.log(re)
+// let p1=document.querySelector('p')
+// let display=()=>{
+// p1.innerHTML="uploded";
+// }
+let p1=document.querySelectorAll('p')
+let display=()=>{
+    p1[0].innerText="uploded";
+    p1[0].style.backgroundColor='red';
+    p1[1].innerText="uploded";
+    p1[1].style.backgroundColor='red';
+    p1[2].innerText="uploded";
+    p1[2].style.backgroundColor='red';
+    alert("hi");
+   }
+
