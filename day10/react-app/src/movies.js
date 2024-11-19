82fc67f6db510c1469817c97a21cdad8d47c866d
sahil-verma-08 <sahilverma2431@gmail.com>
2024-11-19 15:34:57 +0530

    19 nov

---
+++ day10/react-app/src/movies.js
@@ -0,0 +1,40 @@
+const movies=[
+    {
+    id:1,
+    image:" https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/f/r/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfdpjpqg85j5d.jpeg?q=70",
+    title: "TARZAN ",
+    language: " Hindi",
+    cost: 200
+},
+{
+    id:2,
+    image:"https://rukminim2.flixcart.com/image/612/612/av-media/movies/d/6/a/dilwale-dulhania-le-jayenge-dil-to-pagal-hai-original-imadmyghnzenyzwf.jpeg?q=70",
+    title: "DDLJ ",
+    language: " Hindi",
+    cost: 200
+},
+{
+    id:3,
+    image:"https://rukminim2.flixcart.com/image/612/612/av-media/movies/e/n/c/dhoom-3-original-imadvw2ypnsm38at.jpeg?q=70",
+    title: " DHOOM:3",
+    language: " Hindi",
+    cost: 200
+},
+{
+    id:4,
+    image: "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/9/d/d/2013-blu-ray-eagle-hindi-golmaal-original-imagms36uz2hucpy.jpeg?q=70",
+    title: " GOOLMAL ",
+    language: " Hindi ",
+    cost: 200
+},
+{
+    id:5,
+    image: "https://rukminim2.flixcart.com/image/612/612/av-media/movies/q/w/c/gangs-of-wasseypur-i-original-imadnzhqzxzrgumh.jpeg?q=70",
+    title: "GANGS OF VASEPURE ",
+    language: " Hindi",
+    cost: 200
+},
+
+
+];
+export default movies;
